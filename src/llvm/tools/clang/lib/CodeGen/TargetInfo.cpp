56db0f34404d9c18660bd12496242df621922cee
Kristina Bessonova <ch.bessonova@gmail.com>
2018-10-17 10:31:16 +0300

    [MSP430] Improve support of 'interrupt' attribute
    * Accept as an argument constants in range 0..63 (aligned with TI
    headers and linker scripts provided with TI GCC toolchain).
    
    * Emit function attribute 'interrupt'='xx' instead of aliases
    (used in the backend to create section for particular interrupt vector).
    
    * Add more diagnostics.

--- src/llvm/tools/clang/lib/CodeGen/TargetInfo.cpp
+++ src/llvm/tools/clang/lib/CodeGen/TargetInfo.cpp
@@ -6763,21 +6763,19 @@ void MSP430TargetCodeGenInfo::setTargetAttributes(
   if (GV->isDeclaration())
     return;
   if (const FunctionDecl *FD = dyn_cast_or_null<FunctionDecl>(D)) {
-    if (const MSP430InterruptAttr *attr = FD->getAttr<MSP430InterruptAttr>()) {
-      // Handle 'interrupt' attribute:
-      llvm::Function *F = cast<llvm::Function>(GV);
+    const MSP430InterruptAttr *Attr = FD->getAttr<MSP430InterruptAttr>();
+    if (!Attr)
+      return;
 
-      // Step 1: Set ISR calling convention.
-      F->setCallingConv(llvm::CallingConv::MSP430_INTR);
+    // Handle 'interrupt' attribute:
+    llvm::Function *F = cast<llvm::Function>(GV);
 
-      // Step 2: Add attributes goodness.
-      F->addFnAttr(llvm::Attribute::NoInline);
+    // Step 1: Set ISR calling convention.
+    F->setCallingConv(llvm::CallingConv::MSP430_INTR);
 
-      // Step 3: Emit ISR vector alias.
-      unsigned Num = attr->getNumber() / 2;
-      llvm::GlobalAlias::create(llvm::Function::ExternalLinkage,
-                                "__isr_" + Twine(Num), F);
-    }
+    // Step 2: Add attributes goodness.
+    F->addFnAttr(llvm::Attribute::NoInline);
+    F->addFnAttr("interrupt", llvm::utostr(Attr->getNumber()));
   }
 }
 
